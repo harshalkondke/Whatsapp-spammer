e6389e8e4b948e81a352bf5a1196496b7c5a2102
Harshal Kondke <52579686+harshalkondke@users.noreply.github.com>
2020-01-14 15:07:03 +0530

    Update script.py

--- script.py
+++ script.py
@@ -9,32 +9,20 @@ input("Press anything after QR scan")
 time.sleep(5)
 print("loged in")
 
-names = ["Kamothe 🐶"]
+# add names of contact that are saved in your phone as it is. in the list
+names = ["name_of_contact"]
 
 for name in names:
 
     person = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
     person.click()
-
-    # for i in range(1,3):
-    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    #
-    # msg_got = driver.find_elements_by_css_selector("span.selectable-text.invisible-space.copyable-text")
-    # msg = [message.text for message in msg_got]
-    #
-    # if msg[-1] == "Happy New Year":
-    #     reply = driver.find_element_by_class_name("_2S1VP.copyable-text.selectable-text")
-    #     reply.clear()
-    #     reply.send_keys("Same to you! :)")
-    #     reply.send_keys(Keys.RETURN)
-    # else :
-    #     reply = driver.find_element_by_class_name("_2S1VP.copyable-text.selectable-text")
-    #     reply.clear()
-    #     reply.send_keys("Happy New Year! :)")
-    #     reply.send_keys(Keys.RETURN)
-    for i in range(0,10000):
+    
+    # n = number os messages to be send...
+    n = 10000
+    for i in range(0,n):
         reply = driver.find_element_by_class_name("_3u328.copyable-text.selectable-text")
         reply.clear()
-        reply.send_keys("Fuck you...")
+        # message to be send. 
+        reply.send_keys("Hi there...")
         reply.send_keys(Keys.RETURN)
         print(i)
